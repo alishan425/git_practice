125269a02386f72e3d95fd373a5acf045ae29805
Ali Shan <alishanamin425@gmail.com>
2026-06-27 23:10:23 +0500

    Added send_email feature on email branch

--- example.py
+++ example.py
@@ -5,3 +5,8 @@ def add_numbers(a, b):
     return a + b
 
 print("Result: {add_numbers(5, 10)}")
+
+def send_email(user_email):
+    print(f"Sending welcome email to {user_email}...")
+
+send_email("ali@uet.edu.pk")
