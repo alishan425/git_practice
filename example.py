cb4815712993a11f8cf52d028e343cdbb90228b6
Ali Shan <alishanamin425@gmail.com>
2026-06-28 12:05:17 +0500

    remove_print_lines

--- example.py
+++ example.py
@@ -1,5 +1,4 @@
 # example.py
-print("Hello World! This is my first Git project.")
 
 def add_numbers(a, b):
     return a + b
@@ -8,7 +7,3 @@ print("Result: {add_numbers(5, 10)}")
 
 def send_email(user_email):
     print(f"Sending welcome email to {user_email}...")
-
-send_email("ali@uet.edu.pk")
-
-print("Hello AliShan You are practicing Pull Request Today. You are doing great. Keep it up.")
